9daea6d7fb19ce015be13a20c4ce4158ec5a4056
georgi2313 <georgikjoseph@gmail.com>
2019-07-19 09:27:53 +0530

    added: post_share (try:3)

--- webapp/static/js/share.js
+++ webapp/static/js/share.js
@@ -1,13 +1,16 @@
 const shareButton = document.querySelector('.share-button');
 const shareDialog = document.querySelector('.share-dialog');
 const closeButton = document.querySelector('.close-button');
+const shareData = { files: 'https://postit.pythonanywhere.com/media/images/pythonanywhere.jpg' };
+
 
 shareButton.addEventListener('click', event => {
   if (navigator.share) { 
    navigator.share({
       title: 'PostIT (title)',
       text: 'sample text',
-      url: 'https://postit.pythonanywhere.com/media/images/pythonanywhere.jpg',
+      url: 'https://postit.pythonanywhere.com',
+      files: 'https://postit.pythonanywhere.com/media/images/pythonanywhere.jpg', 
     }).then(() => {
       console.log('Thanks for sharing!');
     })
