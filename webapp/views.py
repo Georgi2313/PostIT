2c10125f8cce0255c76eb456a3e3fbd4863f0d2e
georgi2313 <georgikjoseph@gmail.com>
2019-07-21 00:00:13 +0530

    bugfixes: auto login after signup(try:2)

--- webapp/views.py
+++ webapp/views.py
@@ -14,10 +14,10 @@ def signup(request):
     if request.method == 'POST' :
         if form.is_valid():
             form.save()
-            username = self.request.POST['username']
-            password = self.request.POST['password1']
+            username = request.POST['username']
+            password = request.POST['password1']
             user = authenticate(username=username, password=password)
-            login(self.request, user)
+            login(request, user)
             return redirect('home')
         else:
             form= AccountForm()
